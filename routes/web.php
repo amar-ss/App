0ca89e13a32aca848e4fd62ccb6f09bb5ecc4318
amar-ss <120350112+amar-ss@users.noreply.github.com>
2023-01-30 09:27:40 +0700

    membuat route artis

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HairartisController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route:midleware('auth')-> group(function(){
+    //manajemen hairartis
+    Route:get('/hairartis', [HairartisController::class, 'index']);
+})
